6cfbd88654dc204c054839dba7f24f50f28a6ff6
Hbence10 <93594678+Hbence10@users.noreply.github.com>
2022-05-12 14:16:44 +0200

    finish

---
+++ algoritmus.js
@@ -0,0 +1,26 @@
+const gomb = document.getElementById('jatek');
+//Ha a gombra kattintok akkor a Jatek fügvény fusson le
+gomb.addEventListener("click", Jatek);
+
+
+function Jatek (){
+    let szam = Math.floor(Math.random() * 10) + 1;
+    let sz = 1; 
+    let tipp = prompt("Adj meg egy számot: ");
+
+    while (tipp != szam) {
+        if (tipp < szam) {
+            alert("nagyobb")
+        } else {
+            alert("kisebb")
+        }
+        //tippek számának növelése
+        sz++;
+
+        //Következő tipp
+        tipp = prompt("Adjad meg mégegyszer")
+    }
+
+    let eredmeny = document.getElementById('eredmeny');
+    eredmeny.innerHTML = "Eltaláltad! Probálkozások száma" + sz;
+}
